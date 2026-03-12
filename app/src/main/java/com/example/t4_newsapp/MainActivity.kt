9ad20deea2459b4481c9b67e92b9044849e4d6a6
Diego Tierra <dgaeltfp005@gmail.com>
2026-03-11 22:20:33 -0600

    empiezo la estructura principal, seccion de buscador y seccion de pestañas

--- app/src/main/java/com/example/t4_newsapp/MainActivity.kt
+++ app/src/main/java/com/example/t4_newsapp/MainActivity.kt
@@ -38,19 +38,59 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-// 2. COLOR PERSONALIZADO (Definimos el color morado una vez)
+
 val MiColorMorado = Color(0xFF6E56F8)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PantallaNoticias() {
-    // 3. DATOS DE PRUEBA (Asumiendo que tienes estas imágenes en res/drawable)
+
     val listaNoticias = listOf(
         Noticia("El presidente de EE.UU. no muestra signos de arrepentimiento...", "febrero 08 - 2024", R.drawable.trump),
         Noticia("Bañarse en la piscina del desierto de Cleopatra", "febrero 09 - 2024", R.drawable.cleopatra),
         Noticia("Gigantes tecnológicos e IA", "febrero 10 - 2024", R.drawable.ia),
         Noticia("El rover de Marte envía datos", "febrero 11 - 2024", R.drawable.marte)
     )
+    // 4. ESTRUCTURA PRINCIPAL (Columna con scroll)
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp, vertical = 10.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp) // Espacio entre cada sección
+    ) {
+
+        // --- SECCIÓN: BUSCADOR ---
+        item {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    // Redondeamos los bordes completamente
+                    .clip(RoundedCornerShape(50.dp)),
+                placeholder = { Text("Buscar", color = Color.Gray) },
+                // 5. ESTILO DEL BUSCADOR: Color morado para el borde
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = MiColorMorado,
+                    focusedBorderColor = MiColorMorado
+                )
+            )
+        }
+
+        // --- SECCIÓN: PESTAÑAS (Noticias, Eventos, Clima) ---
+        item {
+            Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
+                // Pestaña "Noticias" (Seleccionada)
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Noticias", fontWeight = FontWeight.ExtraBold, fontSize = 24.sp, color = Color.Black)
+                    // La línea morada debajo
+                    Box(Modifier.width(40.dp).height(4.dp).background(MiColorMorado))
+                }
+                // Pestañas no seleccionadas
+                Text("Eventos", color = Color.LightGray, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Text("Clima", color = Color.LightGray, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            }
+        }
 
 
 
